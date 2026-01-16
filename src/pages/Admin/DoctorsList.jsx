eb5adeadfc11f9366b1a4e477590801040106859
Mahasin Prodhan <mahasinprodhan2@gmail.com>
2026-01-17 04:23:59 +0530

    Display all doctors in admin panel

--- src/pages/Admin/DoctorsList.jsx
+++ src/pages/Admin/DoctorsList.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { AdminContext } from "../../context/AdminContext";
 
-const DoctorsList = () => {
-  return <div>DoctorsList</div>;
+const DoctorList = () => {
+  const { getAllDoctors, doctors, aToken, changeAvailability } =
+    useContext(AdminContext);
+
+  useEffect(() => {
+    getAllDoctors();
+  }, [aToken]);
+  return (
+    <div>
+      <div className="grid grid-cols-5 gap-4 px-3 pt-5 mx-10 sm:px-0 ">
+        {doctors &&
+          doctors.map((item, index) => (
+            <div
+              key={index}
+              className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-6px] transition-all duration-300"
+            >
+              <img
+                className="bg-blue-50 hover:bg-blue-400"
+                src={item.image}
+                alt={item.name}
+              />
+              <div className="p-4">
+                <div className="flex items-center gap-2 text-center text-gray-600">
+                  <input
+                    type="checkbox"
+                    name=""
+                    id=""
+                    checked={item.available}
+                    onChange={() => changeAvailability(item._id)}
+                  />
+                  <p>Available</p>
+                </div>
+                <p className="text-lg font-medium text-gray-900 ">
+                  {item.name}
+                </p>
+                <p className="text-sm text-gray-600">{item.speciality}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
-export default DoctorsList;
+export default DoctorList;
